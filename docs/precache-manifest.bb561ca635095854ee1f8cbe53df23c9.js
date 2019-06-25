91d0475f6f9cc76fa495a41c6ec2c8b313849679
irene-gomez <irene@irenegf.com>
2019-06-25 23:57:18 +0200

    Create docs directory

---
+++ docs/precache-manifest.bb561ca635095854ee1f8cbe53df23c9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d326a55f7e6d7c051c259c2abf7cdb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4b210f5e53b2003cea",
+    "url": "/static/css/main.6ecb8b7d.chunk.css"
+  },
+  {
+    "revision": "f21ebe13f8b376a1de9a",
+    "url": "/static/js/2.13569312.chunk.js"
+  },
+  {
+    "revision": "3e4b210f5e53b2003cea",
+    "url": "/static/js/main.85e3b28d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54973a6f41e338769a1144569a24163e",
+    "url": "/static/media/01-rock-code.54973a6f.png"
+  },
+  {
+    "revision": "37ec403603a4bb9aca4206f942845840",
+    "url": "/static/media/02-disena-tu-aventura_02.37ec4036.png"
+  },
+  {
+    "revision": "6f4ffcb3adfd8df8f31bf3924bac0a99",
+    "url": "/static/media/03-codigo-heredado.6f4ffcb3.png"
+  },
+  {
+    "revision": "382ce87e200e1cf19e814d396c041b62",
+    "url": "/static/media/04-eventbrite.382ce87e.png"
+  },
+  {
+    "revision": "cfe88b02a6c1bc610b804fd24dfe4eaa",
+    "url": "/static/media/05-harry-potter-character.cfe88b02.png"
+  },
+  {
+    "revision": "cd3ff4a82ca2ed19a1fd1ee284553313",
+    "url": "/static/media/avatar.cd3ff4a8.jpeg"
+  },
+  {
+    "revision": "6334181a4244d0b812e6a1dbcfb7e0a9",
+    "url": "/static/media/close.6334181a.svg"
+  },
+  {
+    "revision": "c57f0aea0c19f71407cfdb1656543136",
+    "url": "/static/media/email.c57f0aea.png"
+  },
+  {
+    "revision": "8b1fc849a58d3fe7e53b0730cf2601fb",
+    "url": "/static/media/github.8b1fc849.png"
+  },
+  {
+    "revision": "39aa7dd14e9c223237df46f320b268be",
+    "url": "/static/media/linkedin.39aa7dd1.png"
+  },
+  {
+    "revision": "4345c2d88030d5c06074a77b91dfce10",
+    "url": "/static/media/menu.4345c2d8.svg"
+  },
+  {
+    "revision": "04c305ce1c56a0af4d173e8fedcf37f3",
+    "url": "/static/media/skills.04c305ce.svg"
+  },
+  {
+    "revision": "05656efa768d0d89077966eaf926eaa9",
+    "url": "/static/media/twitter.05656efa.png"
+  }
+]);
