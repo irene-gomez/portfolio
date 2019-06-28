b143e54594dd358032759e468b65063e2a39fec8
irene-gomez <irene@irenegf.com>
2019-06-28 10:25:38 +0200

    Create docs directory

---
+++ docs/precache-manifest.b09bd76cdb7dd4bc5684e9bac1bbfe4d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8604edc36e2e67f7d8b57e28825c832b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "391be3d0c6f667c761d5",
+    "url": "./static/css/main.0de9168e.chunk.css"
+  },
+  {
+    "revision": "f21ebe13f8b376a1de9a",
+    "url": "./static/js/2.13569312.chunk.js"
+  },
+  {
+    "revision": "391be3d0c6f667c761d5",
+    "url": "./static/js/main.ea7cf27d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "54973a6f41e338769a1144569a24163e",
+    "url": "./static/media/01-rock-code.54973a6f.png"
+  },
+  {
+    "revision": "37ec403603a4bb9aca4206f942845840",
+    "url": "./static/media/02-disena-tu-aventura_02.37ec4036.png"
+  },
+  {
+    "revision": "6f4ffcb3adfd8df8f31bf3924bac0a99",
+    "url": "./static/media/03-codigo-heredado.6f4ffcb3.png"
+  },
+  {
+    "revision": "382ce87e200e1cf19e814d396c041b62",
+    "url": "./static/media/04-eventbrite.382ce87e.png"
+  },
+  {
+    "revision": "cfe88b02a6c1bc610b804fd24dfe4eaa",
+    "url": "./static/media/05-harry-potter-character.cfe88b02.png"
+  },
+  {
+    "revision": "cd3ff4a82ca2ed19a1fd1ee284553313",
+    "url": "./static/media/avatar.cd3ff4a8.jpeg"
+  },
+  {
+    "revision": "c57f0aea0c19f71407cfdb1656543136",
+    "url": "./static/media/email.c57f0aea.png"
+  },
+  {
+    "revision": "8b1fc849a58d3fe7e53b0730cf2601fb",
+    "url": "./static/media/github.8b1fc849.png"
+  },
+  {
+    "revision": "39aa7dd14e9c223237df46f320b268be",
+    "url": "./static/media/linkedin.39aa7dd1.png"
+  },
+  {
+    "revision": "5ca1eee149f7b7d715e86601d4c550f5",
+    "url": "./static/media/skills.5ca1eee1.png"
+  },
+  {
+    "revision": "05656efa768d0d89077966eaf926eaa9",
+    "url": "./static/media/twitter.05656efa.png"
+  }
+]);
